74f027f624b973634b3b80af351fbdee478e96b7
Iamneo <techadmin@iamneo.ai>
2023-10-03 06:40:34 +0000

    Backup Commit

---
+++ nodeapp/server.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const getReq = require("./methods/getRequest");
+const postReq = require("./methods/postRequest");
+const putReq = require("./methods/putRequest");
+const deleteReq = require("./methods/deleteRequest");
+let laptops = require("./data/laptops.json");
+//require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  req.laptops = laptops;
+  switch (req.method) {
+    case "GET":
+      getReq(req, res);
+      break;
+    case "POST":
+      postReq(req, res);
+      break;
+    case "PUT":
+      putReq(req, res);
+      break;
+    case "DELETE":
+      deleteReq(req, res);
+      break;
+    default:
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "application/json");
+      res.write(
+        JSON.stringify({ title: "Not Found", message: "Route not found" })
+      );
+      res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started on port : ${PORT}`);
+});
+module.exports = server;
